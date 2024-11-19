46a08a3f9a91c3a1be1300d05f6eba5606aef021
eddydpan <eddydotpan@gmail.com>
2024-11-19 14:25:10 -0500

    Add activation implementation to jarvis

--- jarvis.py
+++ jarvis.py
@@ -4,10 +4,14 @@ from pydub import AudioSegment
 from pydub.playback import play
 from groq import Groq
 import env
+import serial
+
 
 FS = 44100  # Sample rate
 SECONDS = 2  # Duration of recording
 OUTPUT_PATH = "audio/output.wav"
+arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=0.1)
+
 
 # Initialize the Groq client
 client = Groq(
@@ -44,23 +48,37 @@ def transcribe():
 
 
 while True:
+	
     print("started recording...")
     record_audio()
     print("stop recording...")
     command = transcribe()
-    if "open" in command and "helmet" in command:
-        # Open helmet with servo
-        print("helmet opened")
-        # Play audio
-        sound = AudioSegment.from_file("audio/open_helmet.mp3")
-        play(sound)
+    if(not activated):
+		if "jarvis" in command and "activate" in command:
+			activated = True
+			print("activate jarvis")
+			# Play audio
+			sound1 = AudioSegment.from_file("audio/jarvis_activate.mp3")
+			sound2 = AudioSegment.from_file("audio/iron_man_repulsor.mp3")
+			play(sound1)
+			play(sound2)
+			arduino.write(bytes('Activate\n'))
+    elif(activated):
+		if "open" in command and "helmet" in command:
+			# Open helmet with servo
+			print("helmet opened")
+			# Play audio
+			sound = AudioSegment.from_file("audio/open_helmet.mp3")
+			play(sound)
+			arduino.write(bytes(b'Open helmet\n'))
 
-    elif "close" in command and "helmet" in command:
-        # Close helmet with servo
-        print("helmet closed")
-        # Play audio
-        sound = AudioSegment.from_file("audio/close_helmet.mp3")
-        play(sound)
-    elif "bye" in command:
-        print("bye bye!")
-        break
+		elif "close" in command and "helmet" in command:
+			# Close helmet with servo
+			print("helmet closed")
+			# Play audio
+			sound = AudioSegment.from_file("audio/close_helmet.mp3")
+			play(sound)
+			arduino.write(bytes(b'Close helmet\n'))
+		elif "bye" in command:
+			print("bye bye!")
+			break
